80ca324b9e9bc61efc8e8986b1d677a6878521a8
yjchoi1546 <yjchoi2000@ajou.ac.kr>
2025-07-17 21:44:23 +0900

    Sol) level 1 1문제

---
+++ Level1/sep_x_with_n_number.py
@@ -0,0 +1,23 @@
+# 문제 설명
+# 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+# 제한 조건
+## x는 -10000000 이상, 10000000 이하인 정수입니다.
+## n은 1000 이하인 자연수입니다.
+
+# 내 답
+def solution(x, n):
+    result = []
+
+    for i in range(1,n+1):
+        result.append(x*i)
+
+    return result
+
+# 다른 사람 풀이
+
+def number_generator(x, n):
+    # 함수를 완성하세요
+    return [i * x + x for i in range(n)]
+
+print(number_generator(2, 5))
